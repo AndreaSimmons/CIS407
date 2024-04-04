7e11964b7d259483d1cdb971891d4d9bd48275bf
AndreaSimmons <andrealcariola@gmail.com>
2024-04-04 08:51:38 -0400

    Create Week1LabEnvironment.java

---
+++ Week1LabEnvironment.java
@@ -0,0 +1,10 @@
+package week1LabEnvironment;
+
+public class Week1LabEnvironment {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hellow World");
+    System.out.println();
+    System.out.println("Congratulations on your first Java program");
+}
